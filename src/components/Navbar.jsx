d337d591a6cec09ab7b5a2293aabbbad3a8b6c8d
Santiago Calderón <santi.calderon.sc@gmail.com>
2022-11-23 20:13:30 -0300

    iniciando repositorio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+function Navbar () {
+  return (
+    <nav className="nav">
+      <h1>Negocio</h1>
+
+    <ul>
+      <li>
+        <a href="">Seccion 1</a>
+      </li>
+      <li>
+        <a href="">Seccion 2</a>
+      </li>
+      <li>
+        <a href="">Seccion 3</a>
+      </li>
+      <li>
+        <a href="">Seccion 4</a>
+      </li>
+      <li>
+        <a href="">&#128722;</a>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
+
+export default Navbar;
